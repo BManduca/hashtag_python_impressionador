ae8457be1fa5111de11c22e2df47e0e790833178
Brunno Manduca <brunnomanducarfe@gmail.com>
2025-03-13 21:49:25 -0300

    feat: Integração Python - SQL | resolvendo desafio envolvendo tkinter, database e principios do CRUD em diretamente no banco de dados

--- módulos/módulo28/Modulo28 Aulas12_13_14_15_16_17/Python e SQL - Controle de Estoque.py
+++ módulos/módulo28/Modulo28 Aulas12_13_14_15_16_17/Python e SQL - Controle de Estoque.py
@@ -1,25 +1,138 @@
 from tkinter import *
+from datetime import datetime
+import pyodbc
+
+
+######## criando conexao #############
+
+dados_conexao = (
+    "Driver={SQLite3 ODBC Driver};"
+    "Server=localhost;"
+    "Database=Estoque.db;"
+)
+
+conexao = pyodbc.connect(dados_conexao)
+
+cursor = conexao.cursor()
 
 ######## funcionalidades do sistema #############
 
+# print(caixa_texto.get('1.0', END))
+# print(nome_insumo.get())
+# print(data_insumo.get())
+# print(lote_insumo.get())
+# print(qtde_insumo.get())
+
 def adicionar_insumo():
-    print(caixa_texto.get('1.0', END))
-    print(nome_insumo.get())
-    print(data_insumo.get())
-    print(lote_insumo.get())
-    print(qtde_insumo.get())
+
+    if not nome_insumo.get().strip() or not qtde_insumo.get().strip() or not data_insumo.get() or not lote_insumo.get().strip():
+        # exibir uma mensagem -> nome insumo inválido
+        # deletar tudo na caixa de texto
+        caixa_texto.delete("1.0", END)
+
+        # escrever na caixa de texto
+        caixa_texto.insert("1.0", "Nome, Quantidade, Data de validade ou Lote do insumo inválido!")
+
+        # finalizar a função
+        return
+
+    # verificando se a data está no formato yyyy-mm-dd
+    try:
+        datetime.strptime(data_insumo.get(), '%Y-%m-%d') # convertendo a string em data
+    except ValueError:
+        caixa_texto.delete("1.0", END)
+        caixa_texto.insert("1.0", "Data de validades inválida! Use o formato yyyy-mm-dd")
+        return
+
+    if len(data_insumo.get()) < 2:
+        caixa_texto.delete("1.0", END)
+
+        caixa_texto.insert("1.0", "Data de validade inválida!")
+
+        return
+
+    cursor.execute('''
+        INSERT INTO Estoque (Produto, Quantidade, DataValidade, Lote) 
+                   VALUES(?, ?, ?, ?)
+    ''', (nome_insumo.get(), qtde_insumo.get(), data_insumo.get(), lote_insumo.get()))
+
+    cursor.commit()
     
     # deletar tudo da caixa de texto
     caixa_texto.delete("1.0", END)
     
     # escrever na caixa de texto
-    caixa_texto.insert("1.0", "Texto")
+    caixa_texto.insert("1.0", f"Insumo {nome_insumo.get()} adicionado com sucesso!")
     
 def deletar_insumo():
-    print("deletar_insumo")
+    if not nome_insumo.get().strip() or not lote_insumo.get().strip():
+        # exibir uma mensagem -> nome insumo inválido
+        # deletar tudo na caixa de texto
+        caixa_texto.delete("1.0", END)
+
+        # escrever na caixa de texto
+        caixa_texto.insert("1.0", "Nome ou Lote do insumo inválido!")
+
+        # finalizar a função
+        return
+    
+    # Buscando insumo no DB
+    cursor.execute('''
+        SELECT * FROM Estoque
+                   WHERE Produto = ? AND Lote = ?
+    ''', (nome_insumo.get(), lote_insumo.get(), ))
+
+    resultado_busca = cursor.fetchone()
+
+    if resultado_busca is None:
+
+        # limpando a caixa de texto
+        caixa_texto.delete("1.0", END)
+
+        # mensagem de retorno que insumo não foi localizado
+        caixa_texto.insert("1.0", f"Insumo '{nome_insumo.get()}' pertencente ao Lote '{lote_insumo.get()}' não foi localizado no estoque!")
+        return
+    
+    #  caso encontrar o insumo, deletar o mesmo
+    cursor.execute('''
+        DELETE FROM Estoque
+                   WHERE Produto = ? AND Lote = ?
+    ''', (nome_insumo.get(), lote_insumo.get(),))
+    cursor.commit()
+
+    # limpar a caixa de texto
+    caixa_texto.delete("1.0", END)
+
+    # mensagem de retorno na caixa de texto
+    caixa_texto.insert("1.0", f"Insumo {nome_insumo.get()} pertencente ao Lote {lote_insumo.get()} foi deletado com sucesso!")
 
 def consumir_insumo():
-    print("consumir_insumo")
+    if len(nome_insumo.get()) < 2 or len(lote_insumo.get()) < 1 or len(qtde_insumo.get()) < 1:
+        # exibir uma mensagem -> nome insumo inválido
+        # deletar tudo na caixa de texto
+        caixa_texto.delete("1.0", END)
+
+        # escrever na caixa de texto
+        caixa_texto.insert("1.0", "Nome, Lote ou Quantidade do insumo está inválido!")
+
+        # finalizar a função
+        return
+    
+    #  caso encontrar o insumo, consumir o mesmo
+    cursor.execute('''
+        UPDATE Estoque
+                   SET Quantidade = Quantidade - ?
+                   WHERE Produto = ? AND Lote = ?
+    ''', (qtde_insumo.get(), nome_insumo.get(), lote_insumo.get()))
+
+    cursor.commit()
+
+    # limpar a caixa de texto
+    caixa_texto.delete("1.0", END)
+
+    # mensagem de retorno na caixa de texto
+    caixa_texto.insert("1.0", f"A quantidade de {qtde_insumo.get()} unidade(s) do insumo {nome_insumo.get()} foi consumido!")
+
 
 def visualizar_insumo():
     print("visualizar_insumo")
@@ -107,6 +220,7 @@ entry0_bg = canvas.create_image(
 caixa_texto = Text(
     bd = 0,
     bg = "#ffffff",
+    fg="#000000",
     highlightthickness = 0)
 
 caixa_texto.place(
@@ -122,6 +236,7 @@ entry1_bg = canvas.create_image(
 nome_insumo = Entry(
     bd = 0,
     bg = "#ffffff",
+    fg="#000000",
     highlightthickness = 0)
 
 nome_insumo.place(
@@ -137,6 +252,7 @@ entry2_bg = canvas.create_image(
 data_insumo = Entry(
     bd = 0,
     bg = "#ffffff",
+    fg="#000000",
     highlightthickness = 0)
 
 data_insumo.place(
@@ -152,6 +268,7 @@ entry3_bg = canvas.create_image(
 lote_insumo = Entry(
     bd = 0,
     bg = "#ffffff",
+    fg="#000000",
     highlightthickness = 0)
 
 lote_insumo.place(
@@ -167,6 +284,7 @@ entry4_bg = canvas.create_image(
 qtde_insumo = Entry(
     bd = 0,
     bg = "#ffffff",
+    fg="#000000",
     highlightthickness = 0)
 
 qtde_insumo.place(
@@ -176,3 +294,6 @@ qtde_insumo.place(
 
 window.resizable(False, False)
 window.mainloop()
+
+cursor.close()
+conexao.close()
